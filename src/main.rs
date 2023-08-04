e885a11444a3d40d28c93d66340f4e092bdb1708
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2023-08-04 00:14:22 -0400

    feat(bin): option to add utc TZ to ts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::env;
 
 fn help() {
-    println!("usage: uts2ts SECONDS");
+    println!("usage: uts2ts SECONDS [utc|u]");
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        2 => {
+        2 | 3 => {
             let num = &args[1];
 
             let number: i64 = match num.parse() {
@@ -20,7 +20,13 @@ fn main() {
                 }
             };
             let ts = uts2ts::uts2ts(number);
-            println!("{}", ts.as_string());
+            if args.len() == 2 {
+                println!("{}", ts.as_string());
+            } else if args.len() == 3 && (&args[2] == "utc" || &args[2] == "u") {
+                println!("{}", ts.as_string_utc());
+            } else {
+                help();
+            }
         }
         _ => {
             help();
